1d17a7be69ddef2e82aae9dcccd9dad906314d88
NHuy165 <huyquach165@gmail.com>
2026-06-11 21:03:14 +0700

    feat(BE): Tạo endpoint để ping.

--- backend/src/routes/dev_tools.py
+++ backend/src/routes/dev_tools.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from backend.src.core.database import reset_database
+from backend.src.core.dependencies import SessionDep, select
 
 router = APIRouter()
 
@@ -8,3 +9,8 @@ router = APIRouter()
 @router.post("/wipe-db", status_code=204)
 async def wipe_database():
     await reset_database()
+
+
+@router.get("/ping", status_code=204)
+async def ping(session: SessionDep):
+    await session.execute(select(1))
