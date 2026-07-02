1cc809943dcc8e665b2a03129ff25de2a5f61dbe
NHuy165 <huyquach165@gmail.com>
2026-07-02 18:46:31 +0700

    fix(FE): Logging out now properly clears tanstack cache.

--- frontend/src/features/auth/components/LogoutButton.tsx
+++ frontend/src/features/auth/components/LogoutButton.tsx
@@ -1,12 +1,15 @@
 import { useTokenStore } from '@/features/auth/stores/useTokenStore';
+import { useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 export const LogoutButton = () => {
   const setToken = useTokenStore((state) => state.setToken);
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const handleClick = () => {
     setToken(null);
+    queryClient.clear();
     navigate('/auth');
   };
 
