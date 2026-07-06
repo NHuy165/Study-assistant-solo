bc6bde926721a46dc1684a573407aeec91c4e3fb
NHuy165 <huyquach165@gmail.com>
2026-07-06 12:01:04 +0700

    fix(FE): Fixed zod schema still validating an object instead of an array output when creating study assessments.

--- frontend/src/features/study-progress/api/study-assessment/useCreateStudyAssessment.ts
+++ frontend/src/features/study-progress/api/study-assessment/useCreateStudyAssessment.ts
@@ -5,30 +5,32 @@ import {
 import { apiFetchProtected } from '@/lib/api';
 import { ResponseErrorSchema } from '@/types/error';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { z } from 'zod';
 
-const createStudyAssessmentRequest =
-  async (): Promise<StudyAssessmentOutput> => {
-    // Sends the request and catches operational errors
-    const options = {
-      method: 'POST',
-    };
-
-    const response = await apiFetchProtected(
-      '/study-progress/study-assessment',
-      options,
-    );
-    const rawData = await response.json();
-
-    // Catches backend response errors
-    if (!response.ok) {
-      const validatedError = ResponseErrorSchema.parse(rawData);
-      throw new Error(validatedError.message);
-    }
-
-    // Returns
-    const validatedData = StudyAssessmentOutputSchema.parse(rawData);
-    return validatedData;
+const createStudyAssessmentRequest = async (): Promise<
+  StudyAssessmentOutput[]
+> => {
+  // Sends the request and catches operational errors
+  const options = {
+    method: 'POST',
   };
+
+  const response = await apiFetchProtected(
+    '/study-progress/study-assessment',
+    options,
+  );
+  const rawData = await response.json();
+
+  // Catches backend response errors
+  if (!response.ok) {
+    const validatedError = ResponseErrorSchema.parse(rawData);
+    throw new Error(validatedError.message);
+  }
+
+  // Returns
+  const validatedData = z.array(StudyAssessmentOutputSchema).parse(rawData);
+  return validatedData;
+};
 
 export const useCreateStudyAssessment = () => {
   const queryClient = useQueryClient();
