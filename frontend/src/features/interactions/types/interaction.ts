3cd9f0bf21bea56488cfd6643ee065e7ae104d1c
NHuy165 <huyquach165@gmail.com>
2026-07-19 21:04:07 +0700

    feat(FE): Updated frontend interaction type to reflect the new change to interaction constraint (name can't be empty).

--- frontend/src/features/interactions/types/interaction.ts
+++ frontend/src/features/interactions/types/interaction.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 // ----- INPUT ----- //
 
 export const InteractionInputSchema = z.object({
-  name: z.string(),
-  description: z.string().optional(),
+  name: z.string().min(1),
+  description: z.string(),
 });
 
 export type InteractionInput = z.infer<typeof InteractionInputSchema>;
@@ -12,7 +12,7 @@ export type InteractionInput = z.infer<typeof InteractionInputSchema>;
 // ----- BACKEND OUTPUT ----- //
 
 export const InteractionOutputSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1),
   description: z.string(),
   id: z.int(),
   created_at: z.iso.datetime(),
@@ -23,7 +23,7 @@ export type InteractionOutput = z.infer<typeof InteractionOutputSchema>;
 // ----- UPDATE ----- //
 
 export const InteractionUpdateSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1),
   description: z.string(),
 });
 
