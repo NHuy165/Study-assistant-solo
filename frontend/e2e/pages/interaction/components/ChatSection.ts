aa372df9bfe822f199140edaf6dcae86aa081deb
NHuy165 <huyquach165@gmail.com>
2026-07-22 12:39:47 +0700

    test(E2E): Wrote success E2E tests for the chat section.

--- frontend/e2e/pages/interaction/components/ChatSection.ts
+++ frontend/e2e/pages/interaction/components/ChatSection.ts
@@ -3,8 +3,27 @@ import { expect, type Locator } from '@playwright/test';
 export class ChatSection {
   readonly rootLocator: Locator;
 
+  // Chat window
+  readonly chatWindow: Locator;
+  readonly infoNoChat: Locator;
+  readonly chatConversation: Locator;
+
+  // Chat form
+
+  readonly chatButton: Locator;
+  readonly chatInput: Locator;
+
   constructor(rootLocator: Locator) {
     this.rootLocator = rootLocator;
+
+    // Chat window
+    this.chatWindow = rootLocator.locator('section');
+    this.infoNoChat = this.chatWindow.getByText('No chat history to show.');
+    this.chatConversation = this.chatWindow.getByRole('listitem');
+
+    // Chat form
+    this.chatButton = rootLocator.getByRole('button', { name: 'Send' });
+    this.chatInput = rootLocator.locator('textarea[name="prompt"]');
   }
 
   checkLoaded = async () => {
---
+++ frontend/e2e/tests/chat/chat-success.spec.ts
@@ -0,0 +1,111 @@
+import test, { expect } from '@playwright/test';
+import { createInteraction } from '@e2e/helpers/interactions/create-interaction';
+import interactionData from '@e2e/data/interactions/interaction.json' with { type: 'json' };
+import mockChatData from '@e2e/data/chat/mock-chat.json' with { type: 'json' };
+import userData from '@e2e/data/auth/user.json' with { type: 'json' };
+import { resetDatabase } from '@e2e/helpers/database';
+import { registerUser } from '@e2e/helpers/auth/register-user';
+import { loginUser } from '@e2e/helpers/auth/login-user';
+import { InteractionPage } from '@e2e/pages/interaction/InteractionPage';
+
+test.describe('Chat - Success tests', () => {
+  test.beforeEach(async ({ page, request }) => {
+    await resetDatabase(request);
+
+    // Registers and logs in
+    const user = userData.user;
+
+    await registerUser({ request, user });
+    await loginUser({ user, page });
+
+    // Creates and goes inside interaction
+    const interaction = interactionData.interaction;
+
+    const interactionId = await createInteraction({
+      page,
+      request,
+      interaction,
+    });
+    await page.reload();
+
+    const interactionPage = new InteractionPage(page);
+    await interactionPage.goto(interactionId);
+  });
+
+  test('Check if components are properly loaded.', async ({ page }) => {
+    const chatSection = new InteractionPage(page).chatSection;
+
+    await expect(chatSection.chatButton).toBeVisible();
+    await expect(chatSection.chatInput).toBeVisible();
+    await expect(chatSection.chatWindow).toBeVisible();
+    await expect(chatSection.infoNoChat).toBeVisible();
+  });
+
+  test('Make multiple conversations and check if they are all properly loaded', async ({
+    page,
+  }) => {
+    const chatSection = new InteractionPage(page).chatSection;
+
+    // Builds mock data
+    const prompt1 = 'Test prompt 1';
+    const prompt2 = 'Test prompt 2';
+
+    const interactionId = Number(page.url().split('/').pop());
+
+    const responses = [
+      { ...mockChatData[0], prompt: prompt1, interaction_id: interactionId },
+      { ...mockChatData[1], prompt: prompt2, interaction_id: interactionId },
+    ];
+
+    const staticResponses = [...responses];
+    const history: {
+      prompt: string;
+      interaction_id: number;
+      id: number;
+      answer: string;
+      created_at: string;
+    }[] = [];
+
+    // Mocks response
+    await page.route(`**/api/llm-response/${interactionId}`, async (route) => {
+      const method = route.request().method();
+
+      if (method === 'GET') {
+        await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          json: history,
+        });
+      } else if (method === 'POST') {
+        const mockResponse = responses.shift();
+
+        if (mockResponse) {
+          history.push(mockResponse);
+        }
+
+        await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          json: {
+            ...mockResponse,
+          },
+        });
+      }
+    });
+
+    for (let i = 0; i < mockChatData.length; i++) {
+      // Chats
+      await chatSection.chatInput.fill(staticResponses[i].prompt);
+      await chatSection.chatButton.click();
+
+      await expect(chatSection.chatInput).toBeEmpty();
+      await expect(chatSection.chatConversation).toHaveCount(i + 1);
+      await expect(chatSection.chatConversation.last()).toContainText(
+        `User: ${staticResponses[i].prompt}`,
+      );
+      await expect(chatSection.chatConversation.last()).toContainText(
+        `Chatbot: ${staticResponses[i].answer}`,
+      );
+    }
+  });
+});
