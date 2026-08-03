575d28b89f9908e5c80f8f0563408e25bb962ea5
NHuy165 <huyquach165@gmail.com>
2026-08-03 19:25:34 +0700

    chore(FE): Added vite configuration to allow automatic refresh in development environment when running frontend inside container.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,16 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
     proxy: {
       '/api': {
         target: 'http://backend:8000',
