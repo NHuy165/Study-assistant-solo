5076502c67b6d06158014c4e921b4ecb874b43ff
NHuy165 <huyquach165@gmail.com>
2026-08-07 21:23:43 +0700

    fix(BE): Fixed typo in origins.py

--- backend/src/core/origins.py
+++ backend/src/core/origins.py
@@ -6,7 +6,7 @@ origins = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     # Internal Docker
-    "http://frontend:80"
+    "http://frontend:80",
     # Vercel
     "https://study-assistant-project-psi.vercel.app",
     "https://study-assistant-project-9je5feg3z-nhuy165s-projects.vercel.app",
